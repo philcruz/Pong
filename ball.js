8850500a1ac3834e1162359b847018dcf055cc5b
Phil Cruz <philcruz@gmail.com>
2014-03-07 13:48:31 -0500

    update ball to handle wall collisions

--- ball.js
+++ ball.js
@@ -7,11 +7,22 @@ function Ball(){
     this.x = (game.width/2);
     this.y = game.height/2;
     
-    this.xVelocity = 10;
-    this.yVelocity = 10;
+    this.xVelocity = 0;
+    this.yVelocity = -5;
 }
 
 
 
 Ball.prototype = Object.create(Entity.prototype);
 Ball.prototype.constructor = Ball
+
+//Need to override update function to handle collisions with the upper/lower walls
+Ball.prototype.update = function(){
+    Entity.prototype.update.apply(this,arguments); // like call to super() in Java
+    
+    if (this.y >= game.height - this.height || this.y <= 0)
+        this.yVelocity *= -1; //reverse direction
+        
+    
+        
+}
